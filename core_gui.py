a43c7756af93643024aa4bc94866e19319a4b1b9
yshaarj <39557109+vitus9988@users.noreply.github.com>
2021-01-29 22:44:54 +0900

    build
    ver 1.0.0

--- core_gui.py
+++ core_gui.py
@@ -6,9 +6,8 @@ import webbrowser
 from tkinter import filedialog
 
 
-
 root = Tk()
-root.title("Arcalive Lastorigin Searcher")
+root.title("Arcalive Lastorigin Searcher 1.0.0")
 root.geometry("640x480+600+300")
 root.resizable(False, False)
 
@@ -22,43 +21,45 @@ notebook = ttk.Notebook(root, width=740, height=580)
 notebook.pack()
 
 frame1 = Frame(root)
-notebook.add(frame1, text='공략')
+notebook.add(frame1, text=u"공략")
 frame2 = Frame(root)
-notebook.add(frame2, text='야짤')
+notebook.add(frame2, text=u"야짤")
+frame3 = Frame(root)
+notebook.add(frame3, text=u"창작물(야짤)")
 frame4 = Frame(root)
-notebook.add(frame4, text='Info')
+notebook.add(frame4, text=u"Info")
 
 
 ########################################################################################################################
 ### listbox (공략)
 ########################################################################################################################
 
-listbox = Listbox(frame1, selectmode='single', height=0)
-listbox.pack(side='right', fill='both', expand=True)
+listbox = Listbox(frame1, selectmode="single", height=0)
+listbox.pack(side="right", fill="both", expand=True)
 
 entry_text = Entry(frame1, width=30)
 entry_text.pack()
-entry_text.insert(END, '')
+entry_text.insert(END, "")
 
 radio_value = IntVar()
 
-radio_btn1 = ttk.Radiobutton(frame1, text='제목+내용', variable=radio_value, value=0)
-radio_btn2 = ttk.Radiobutton(frame1, text='제목만', variable=radio_value, value=1)
+radio_btn1 = ttk.Radiobutton(frame1, text=u"제목+내용", variable=radio_value, value=0)
+radio_btn2 = ttk.Radiobutton(frame1, text=u"제목만", variable=radio_value, value=1)
 
 radio_btn1.pack()
 radio_btn2.pack()
-Label(frame1, text='검색어를 입력하세요.\n( ex) 1-1ex , 5-8ex+카엔 )').pack()
+Label(frame1, text=u"검색어를 입력하세요.\n( ex) 1-1ex , 5-8ex+카엔 )").pack()
 
 
 def weblink(*args):
     index = listbox.curselection()[0]
     item = listbox.get(index)
-    if 'https://' in item:
+    if "https://" in item:
         webbrowser.open_new(item)
 
 
 def error():
-    msgbox.showwarning("경고", "검색어를 입력하세요")
+    msgbox.showwarning(u"경고", u"검색어를 입력하세요")
 
 
 def search_btncmd():
@@ -70,73 +71,201 @@ def search_btncmd():
         error()
     else:
         data_arr = cm.NamuliveSearch(radio_value.get(), value)
-        listbox.bind('<<ListboxSelect>>', weblink)
+        listbox.bind("<<ListboxSelect>>", weblink)
         for item in data_arr:
             listbox.insert(END, item)
-            listbox.itemconfig(END,fg='red' if item[0] == 'h' else 'blue')
-        listbox.pack(side='right', fill='both', expand=True)
+            listbox.itemconfig(END, fg="red" if item[0] == "h" else "blue")
+        listbox.pack(side="right", fill="both", expand=True)
 
 
 def search_callback(event):
     search_btncmd()
 
 
-root.bind('<Return>', search_callback)
+root.bind("<Return>", search_callback)
 
 
 ########################################################################################################################
 ### listbox (야짤)
 ########################################################################################################################
 
-yazzal_title = Label(frame2, text='아카라이브 야짤탭 다운로더입니다.')
-yazzal_title.pack(side='top')
-Label(frame2, text='이미지파일은 글번호+글제목 형태의 폴더에 개별저장됩니다.').pack()
-Label(frame2, text='(기본 페이지값은 1입니다.)').pack()
+yazzal_title = Label(frame2, text=u"아카라이브 야짤탭 다운로더입니다.")
+yazzal_title.pack(side="top")
+Label(frame2, text=u"이미지파일은 글번호+글제목 형태의 폴더에 개별저장됩니다.").pack()
+#Label(frame2, text=u"(기본 페이지값은 1입니다.)").pack()
+
+spin_label = Label(frame2, text=u"페이지 수를 입력하세요")
+spin_label.pack()
+
+
+def yazzal_spin_check(self):
+    spin_label.config(text=u"다운받을 페이지를 입력하세요. (1~250)")
+    valid = False
+    if self.isdigit():
+        if int(self) <= 250 and int(self) >= 1:
+            valid = True
+    elif self == '':
+        valid = True
+    return valid
+
+def yazzal_spin_error(self):
+    spin_label.config(text=u"최소 수치는 1\n최대 수치는 250입니다")
+
+
+validate_command = (frame2.register(yazzal_spin_check), "%P")
+invalid_command = (frame2.register(yazzal_spin_error), "%P")
+
+yazzal_spinbox = Spinbox(frame2, from_=1, to=250, validate='all',
+                         validatecommand=validate_command,
+                         invalidcommand=invalid_command)
+yazzal_spinbox.pack()
+
+
+def yazzal_spinbox_value():
+    return yazzal_spinbox.get()
+
+
+
 
 
 def yazzal_error():
-    msgbox.showwarning("경고", "다운로드 폴더 경로를 지정하세요.")
+    msgbox.showwarning(u"경고", u"다운로드 폴더 경로를 지정하세요.")
 
 
 def dir_btncmd():
     down_listbox.delete(0, END)
-    dir_path = filedialog.askdirectory(parent=root, initialdir="/", title='다운로드 파일을 저장할 폴더를 선택하세요')
-    if dir_path == None:
+    dir_path = filedialog.askdirectory(parent=root, initialdir="/", title=u"다운로드 파일을 저장할 폴더를 선택하세요")
+    if dir_path is None:
         pass
     else:
-        down_listbox.insert(END, '{}'.format(dir_path))
-        down_listbox.place(x=80, y=63)
+        down_listbox.insert(END, "{}".format(dir_path))
+        down_listbox.place(x=80, y=103)
 
 
 def down_alarm():
-    msgbox.showwarning("작업완료", "다운로드가 완료되었습니다.")
+    msgbox.showwarning(u"작업완료", u"다운로드가 완료되었습니다.")
 
 
 def down_btncmd():
+    yazzal_page = int(yazzal_spinbox_value())
     try:
         dir_path = down_listbox.get(0, END)[0]
         if len(dir_path) == 0 or len(dir_path) == 1:
             yazzal_error()
         else:
-            cm.created_img_download(dir_path, 1)
+            cm.created_img_download(dir_path, yazzal_page, 1)
             down_alarm()
-    except:
+    except IndexError:
         yazzal_error()
 
-directory_btn = Button(frame2, width=10, text='경로지정', overrelief='solid',command=dir_btncmd)
-down_listbox = Listbox(frame2, selectmode='single', height=0, width=40)
-down_btn = Button(frame2, width=10, text='다운로드 시작', overrelief='solid',command=down_btncmd)
 
-down_listbox.place(x=80, y=63)
-directory_btn.place(x=380, y=60)
-down_btn.place(x=480, y=60)
+directory_btn = Button(frame2, width=10, text=u"경로지정", overrelief="solid",command=dir_btncmd)
+down_listbox = Listbox(frame2, selectmode="single", height=0, width=40)
+down_btn = Button(frame2, width=10, text=u"다운로드 시작", overrelief="solid",command=down_btncmd)
+
+down_listbox.place(x=80, y=103)
+directory_btn.place(x=380, y=100)
+down_btn.place(x=480, y=100)
+
 
 
 ########################################################################################################################
-###info
+### 창작물
 ########################################################################################################################
-Label(frame4, text='Copyright 2021. test1423 All Rights Reserved.').pack(side='bottom')
+#
+yazzal_title = Label(frame3, text=u"아카라이브 창작물(야짤)탭 다운로더입니다.")
+yazzal_title.pack(side="top")
+Label(frame3, text=u"이미지파일은 글번호+글제목 형태의 폴더에 개별저장됩니다.").pack()
+#Label(frame2, text=u"(기본 페이지값은 1입니다.)").pack()
 
+cre_spin_label = Label(frame3, text=u"페이지 수를 입력하세요")
+cre_spin_label.pack()
+
+
+def cre_yazzal_spin_check(self):
+    cre_spin_label.config(text=u"다운받을 페이지를 입력하세요. (1~49)")
+    valid = False
+    if self.isdigit():
+        if int(self) <= 49 and int(self) >= 1:
+            valid = True
+    elif self == '':
+        valid = True
+    return valid
+
+def cre_yazzal_spin_error(self):
+    cre_spin_label.config(text=u"최소 수치는 1\n최대 수치는 49입니다")
+
+
+cre_validate_command = (frame3.register(cre_yazzal_spin_check), "%P")
+cre_invalid_command = (frame3.register(cre_yazzal_spin_error), "%P")
+
+cre_yazzal_spinbox = Spinbox(frame3, from_=1, to=250, validate='all',
+                         validatecommand=cre_validate_command,
+                         invalidcommand=cre_invalid_command)
+cre_yazzal_spinbox.pack()
+
+
+def cre_yazzal_spinbox_value():
+    return cre_yazzal_spinbox.get()
+
+
+def cre_yazzal_error():
+    msgbox.showwarning(u"경고", u"다운로드 폴더 경로를 지정하세요.")
+
+
+def cre_dir_btncmd():
+    cre_down_listbox.delete(0, END)
+    dir_path = filedialog.askdirectory(parent=root, initialdir="/", title=u"다운로드 파일을 저장할 폴더를 선택하세요")
+    if dir_path is None:
+        pass
+    else:
+        cre_down_listbox.insert(END, "{}".format(dir_path))
+        cre_down_listbox.place(x=80, y=103)
+
+
+def cre_down_alarm():
+    msgbox.showwarning(u"작업완료", u"다운로드가 완료되었습니다.")
+
+
+def cre_down_btncmd():
+    cre_yazzal_page = int(cre_yazzal_spinbox_value())
+    try:
+        dir_path = cre_down_listbox.get(0, END)[0]
+        if len(dir_path) == 0 or len(dir_path) == 1:
+            cre_yazzal_error()
+        else:
+            cm.created_img_download(dir_path, cre_yazzal_page, 2)
+            cre_down_alarm()
+    except IndexError:
+        cre_yazzal_error()
+
+
+
+cre_directory_btn = Button(frame3, width=10, text=u"경로지정", overrelief="solid",command=cre_dir_btncmd)
+cre_down_listbox = Listbox(frame3, selectmode=u"single", height=0, width=40)
+cre_down_btn = Button(frame3, width=10, text=u"다운로드 시작", overrelief="solid",command=cre_down_btncmd)
+
+cre_down_listbox.place(x=80, y=103)
+cre_directory_btn.place(x=380, y=100)
+cre_down_btn.place(x=480, y=100)
+
+
+
+########################################################################################################################
+### info
+########################################################################################################################
+
+def git_callback(event):
+    webbrowser.open_new(event.widget.cget("text"))
+
+
+Label(frame4, text="Copyright 2021. vitus9988 All Rights Reserved.").pack(side="bottom")
+git_label = Label(frame4, text=r"https://github.com/vitus9988", fg="blue", cursor="hand2")
+git_label.pack(side="bottom")
+git_label.bind("<Button-1>", git_callback)
+thanks_to = Label(frame4, text=r'Thanks To  SANIC , 섭섭맨').pack(side='bottom')
 
 
 root.mainloop()
+
+# pyinstaller -F core_gui.spec로 빌드
--- core_module.py
+++ core_module.py
@@ -1,36 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 import urllib.request
-from tqdm import tqdm
+import re
 import os
-from moviepy.editor import *
-
 
 def wc(url):
     headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36'}
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
     html = requests.get(url, headers=headers).text
-    soup = BeautifulSoup(html, 'lxml')
+    soup = BeautifulSoup(html, "lxml")
     return soup
 
 
 def NamuliveSearch(radiovalue, keyword):
     global soup, mode
-    mode = ''
-
+    mode = ""
 
     if radiovalue == 0:
-        soup = wc(url='https://arca.live/b/lastorigin?{}category=공략&target=title_content&keyword={}'.format(mode,keyword))
+        soup = wc(url="https://arca.live/b/lastorigin?{}category=공략&target=title_content&keyword={}".format(mode,keyword))
     elif radiovalue == 1:
-        soup = wc(url='https://arca.live/b/lastorigin?{}category=공략&target=title&keyword={}'.format(mode,keyword))
+        soup = wc(url="https://arca.live/b/lastorigin?{}category=공략&target=title&keyword={}".format(mode,keyword))
 
-    detail = soup.find_all('a','vrow')
-    base_url = 'https://arca.live'
+    detail = soup.find_all("a","vrow")
+    base_url = "https://arca.live"
     detail_arr = []
     for i in detail:
         try:
-            link = i.get('href')
-            title = i.find('span','title').text.strip()
+            link = i.get("href")
+            title = i.find("span","title").text.strip()
             detail_arr.append(title)
             detail_arr.append(base_url+link)
         except:
@@ -38,14 +35,15 @@ def NamuliveSearch(radiovalue, keyword):
     return detail_arr
 
 
+
 def RuliwebSearch(keyword):
-    soup = wc(url='https://bbs.ruliweb.com/mobile/game/84992?search_type=subject_content&search_key={}&cate=4'.format(keyword))
-    detail = soup.find_all('tr','table_body')
+    soup = wc(url="https://bbs.ruliweb.com/mobile/game/84992?search_type=subject_content&search_key={}&cate=4".format(keyword))
+    detail = soup.find_all("tr","table_body")
     detail_arr = []
     for i in detail:
         try:
-            title = i.find('div','relative').find('a').text.strip()
-            link = i.find('div','relative').find('a').get('href')
+            title = i.find("div","relative").find("a").text.strip()
+            link = i.find("div","relative").find("a").get("href")
             detail_arr.append(title)
             detail_arr.append(link)
         except:
@@ -53,41 +51,36 @@ def RuliwebSearch(keyword):
     return detail_arr
 
 
-def NavercafeSearch(keyword):
-    pass
-
-
-
 # def img_download(img_path, page_count):
 #
 #     headers = {
-#         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36'}
+#         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
 #     page = 1
 #
 #     while True:
-#         soup = wc(url='https://arca.live/b/lastorigin?category=%EC%95%BC%EC%A7%A4&p={}'.format(page))
-#         detail = soup.find_all('a', 'vrow')
-#         base_url = 'https://arca.live'
+#         soup = wc(url="https://arca.live/b/lastorigin?category=%EC%95%BC%EC%A7%A4&p={}".format(page))
+#         detail = soup.find_all("a", "vrow")
+#         base_url = "https://arca.live"
 #
 #         for i in detail:
 #             try:
-#                 text_num = i.find('span', 'vcol col-id').text.strip()
-#                 link = i.get('href')
-#                 title = i.find('span', 'title').text.strip()
+#                 text_num = i.find("span", "vcol col-id").text.strip()
+#                 link = i.get("href")
+#                 title = i.find("span", "title").text.strip()
 #                 img_url = base_url + link
 #                 yazzl_list = image_link(img_url)
 #
 #                 if len(yazzl_list) != 0:
 #                     count = 1
-#                     if os.path.exists(img_path+'/{}+{}'.format(text_num, title)):
+#                     if os.path.exists(img_path+"/{}+{}".format(text_num, title)):
 #                         pass
-#                     os.makedirs(img_path+'/{}+{}'.format(text_num, title))
+#                     os.makedirs(img_path+"/{}+{}".format(text_num, title))
 #
 #                     for k in yazzl_list:
 #                         request_ = urllib.request.Request(k, None, headers)
 #                         response = urllib.request.urlopen(request_)
-#                         f = open('{}/{}+{}/{}.png'.format(img_path,text_num,title,count),'wb')
-#                         #f = open('{}/{}+{}.png'.format(img_path, count), 'wb')
+#                         f = open("{}/{}+{}/{}.png".format(img_path,text_num,title,count),"wb")
+#                         #f = open("{}/{}+{}.png".format(img_path, count), "wb")
 #                         f.write(response.read())
 #                         f.close()
 #                         count += 1
@@ -101,80 +94,109 @@ def NavercafeSearch(keyword):
 #             break
 
 
-def created_img_download(img_path, page_count):
+def created_img_download(img_path, page_count, tab_mode):
 
     headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36'}
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
     page = 1
-
+    folder_status = True
     while True:
-        soup = wc(url='https://arca.live/b/lastorigin?category=%EC%95%BC%EC%A7%A4&target=all&keyword=&p={}'.format(page))
-        detail = soup.find_all('a', 'vrow')
-        base_url = 'https://arca.live'
+        if tab_mode == 1:
+            soup = wc(url="https://arca.live/b/lastorigin?category=%EC%95%BC%EC%A7%A4&target=all&keyword=&p={}".format(page))
+        else:
+            soup = wc(url="https://arca.live/b/lastorigin?category=%EC%B0%BD%EC%9E%91%EB%AC%BC(%EC%95%BC%EC%A7%A4)&target=all&keyword=&p={}".format(page))
+        detail = soup.find_all("a", "vrow")
+        base_url = "https://arca.live"
 
         for i in detail:
             try:
-                text_num = i.find('span', 'vcol col-id').text.strip()
-                link = i.get('href')
-                title = i.find('span', 'title').text.strip()
+                text_num = i.find("span", "vcol col-id").text.strip()
+                link = i.get("href")
+                retitle = i.find("span", "title").text.strip()
+                #title = re.sub("'[\/:*?'<>|]'", "", retitle)
+                title = re.sub("[-\/=.#?:$}!^*]", "", retitle)
                 img_url = base_url + link
                 yazzl_list = gif_img_link(img_url)
 
                 if len(yazzl_list) != 0:
                     count = 1
-                    if os.path.exists(img_path+'/{}+{}'.format(text_num, title)):
-                        pass
-                    os.makedirs(img_path+'/{}+{}'.format(text_num, title))
+                    if os.path.exists(img_path+"/{}+{}".format(text_num, title)):
+                        folder_status = False
+                        break
+                    os.makedirs(img_path+"/{}+{}".format(text_num, title))
 
                     for k in yazzl_list:
                         request_ = urllib.request.Request(k, None, headers)
                         response = urllib.request.urlopen(request_)
-                        if k[-3:] == 'mp4':
-                            f = open('{}/{}+{}/{}.mp4'.format(img_path, text_num, title, count), 'wb')
+                        if k[-3:] == "mp4":
+                            p = open("{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title), "wt", encoding='utf-8')
+                            p.write("[InternetShortcut]\nURL={}".format(img_url))
+                            p.close()
+                            f = open("{}/{}+{}/{}.mp4".format(img_path, text_num, title, count), "wb")
                             f.write(response.read())
                             f.close()
-                            clip = (VideoFileClip('{}/{}+{}/{}.mp4'.format(img_path, text_num, title, count)))
-                            clip.write_gif('{}/{}+{}/{}.gif'.format(img_path, text_num, title, count))
-
+                            os.rename("{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title),
+                                      "{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title))
+                            # clip = (VideoFileClip("{}/{}+{}/{}.mp4".format(img_path, text_num, title, count)))
+                            # clip.write_gif("{}/{}+{}/{}.gif".format(img_path, text_num, title, count))
+                        elif k[-3:] == "jpg":
+                            p = open("{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title), "wt", encoding='utf-8')
+                            p.write("[InternetShortcut]\nURL={}".format(img_url))
+                            p.close()
+                            f = open("{}/{}+{}/{}.jpg".format(img_path, text_num, title, count), "wb")
+                            f.write(response.read())
+                            f.close()
+                            os.rename("{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title),
+                                      "{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title))
                         else:
-                            f = open('{}/{}+{}/{}.png'.format(img_path, text_num, title, count), 'wb')
+                            p = open("{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title), "wt", encoding='utf-8')
+                            p.write("[InternetShortcut]\nURL={}".format(img_url))
+                            p.close()
+                            f = open("{}/{}+{}/{}.png".format(img_path, text_num, title, count), "wb")
                             f.write(response.read())
                             f.close()
+
+                            os.rename("{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title),
+                                      "{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title))
+
                         count += 1
                 else:
                     pass
             except:
                 continue
         page += 1
-
-        if page > page_count:
+        if page > page_count or folder_status is False:
             break
+
+
+
+
+
 
 
 
 def image_link(borad_link):
     soup = wc(url=borad_link)
-    stat = soup.find('div','fr-view article-content')
-    detail = stat.find_all('img')
-    result = ['https:' + i.get('src') for i in detail]
+    stat = soup.find("div","fr-view article-content")
+    detail = stat.find_all("img")
+    result = ["https:" + i.get("src") for i in detail]
     if len(result) == 0:
         pass
     return result
 
 
+
+
+
+
 def gif_img_link(img_link):
     soup = wc(url=img_link)
-    stat = soup.find('div', 'fr-view article-content')
-    detail = stat.find_all('img')
-    result = ['https:' + i.get('src') for i in detail]
+    stat = soup.find("div", "fr-view article-content")
+    detail = stat.find_all("img")
+    result = ["https:" + i.get("src") for i in detail]
     if len(detail) == 0:
-        gif_detail = stat.find_all('video')
-        result = ['https:' + i.get('src') for i in gif_detail]
+        gif_detail = stat.find_all("video")
+        result = ["https:" + i.get("src") for i in gif_detail]
         return result
     return result
 
-
-
-
-if __name__ == '__main__':
-    board_image_link('C:\yazzal_test')
