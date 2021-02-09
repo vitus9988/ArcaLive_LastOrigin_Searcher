77f441060c31cab07c30d3fa802f32216b18c672
yshaarj <39557109+vitus9988@users.noreply.github.com>
2021-02-10 00:05:01 +0900

    update_ver 1.1.0

--- core_gui.py
+++ core_gui.py
@@ -7,7 +7,7 @@ from tkinter import filedialog
 
 
 root = Tk()
-root.title("Arcalive Lastorigin Searcher 1.0.0")
+root.title("Arcalive Lastorigin Searcher 1.1.0")
 root.geometry("640x480+600+300")
 root.resizable(False, False)
 
@@ -27,7 +27,9 @@ notebook.add(frame2, text=u"야짤")
 frame3 = Frame(root)
 notebook.add(frame3, text=u"창작물(야짤)")
 frame4 = Frame(root)
-notebook.add(frame4, text=u"Info")
+notebook.add(frame4, text=u"창작물")
+frame5 = Frame(root)
+notebook.add(frame5, text=u'Info')
 
 
 ########################################################################################################################
@@ -85,170 +87,308 @@ def search_callback(event):
 root.bind("<Return>", search_callback)
 
 
+
+
 ########################################################################################################################
 ### listbox (야짤)
 ########################################################################################################################
 
-yazzal_title = Label(frame2, text=u"아카라이브 야짤탭 다운로더입니다.")
-yazzal_title.pack(side="top")
-Label(frame2, text=u"이미지파일은 글번호+글제목 형태의 폴더에 개별저장됩니다.").pack()
-#Label(frame2, text=u"(기본 페이지값은 1입니다.)").pack()
 
-spin_label = Label(frame2, text=u"페이지 수를 입력하세요")
-spin_label.pack()
+def edgeObject():
+
+    yazzal_title = Label(frame2, text=u"아카라이브 야짤탭 다운로더입니다.")
+    yazzal_title.pack(side="top")
+    Label(frame2, text=u"이미지파일은 글번호+글제목 형태의 폴더에 개별저장됩니다.").pack()
+
+    cre_spin_label = Label(frame2, text=u"페이지 수를 입력하세요")
+    cre_spin_label.place(x=200, y=70)
+
+    rcm_value = IntVar()
+
+    rcm_btn1 = ttk.Radiobutton(frame2, text=u"전체", variable=rcm_value, value=0)
+    rcm_btn2 = ttk.Radiobutton(frame2, text=u"개념글", variable=rcm_value, value=1)
+
+    rcm_btn1.place(x=140, y=61)
+    rcm_btn2.place(x=140, y=85)
 
 
-def yazzal_spin_check(self):
-    spin_label.config(text=u"다운받을 페이지를 입력하세요. (1~250)")
-    valid = False
-    if self.isdigit():
-        if int(self) <= 250 and int(self) >= 1:
+    def cre_yazzal_spin_check(self):
+
+        lastPage = [250, 51]
+        pageChecker = rcm_value.get()
+        cre_spin_label.config(text=u"다운받을 페이지를\n입력하세요. (1~{})".format(lastPage[pageChecker]))
+        cre_spin_label.place(x=380, y=63)
+        valid = False
+
+        if self.isdigit():
+            if int(self) <= lastPage[pageChecker] and int(self) >= 1:
+                valid = True
+        elif self == '':
             valid = True
-    elif self == '':
-        valid = True
-    return valid
-
-def yazzal_spin_error(self):
-    spin_label.config(text=u"최소 수치는 1\n최대 수치는 250입니다")
+        return valid
 
 
-validate_command = (frame2.register(yazzal_spin_check), "%P")
-invalid_command = (frame2.register(yazzal_spin_error), "%P")
-
-yazzal_spinbox = Spinbox(frame2, from_=1, to=250, validate='all',
-                         validatecommand=validate_command,
-                         invalidcommand=invalid_command)
-yazzal_spinbox.pack()
+    def cre_yazzal_spin_error(self):
+        pageChecker = rcm_value.get()
+        lastPage = [250, 51]
+        cre_spin_label.config(text=u"최소 수치는 1\n최대 수치는 {}입니다".format(lastPage[pageChecker]))
+        cre_spin_label.place(x=380, y=63)
 
 
-def yazzal_spinbox_value():
-    return yazzal_spinbox.get()
+    cre_validate_command = (frame2.register(cre_yazzal_spin_check), "%P")
+    cre_invalid_command = (frame2.register(cre_yazzal_spin_error), "%P")
+
+    cre_yazzal_spinbox = Spinbox(frame2, from_=1, to=250, validate='all',
+                             validatecommand=cre_validate_command,
+                             invalidcommand=cre_invalid_command)
+    cre_yazzal_spinbox.place(x=220, y=73)
 
 
+    def cre_yazzal_spinbox_value():
+        return cre_yazzal_spinbox.get()
 
 
-
-def yazzal_error():
-    msgbox.showwarning(u"경고", u"다운로드 폴더 경로를 지정하세요.")
-
-
-def dir_btncmd():
-    down_listbox.delete(0, END)
-    dir_path = filedialog.askdirectory(parent=root, initialdir="/", title=u"다운로드 파일을 저장할 폴더를 선택하세요")
-    if dir_path is None:
-        pass
-    else:
-        down_listbox.insert(END, "{}".format(dir_path))
-        down_listbox.place(x=80, y=103)
+    def cre_yazzal_error():
+        msgbox.showwarning(u"경고", u"다운로드 폴더 경로를 지정하세요.")
 
 
-def down_alarm():
-    msgbox.showwarning(u"작업완료", u"다운로드가 완료되었습니다.")
-
-
-def down_btncmd():
-    yazzal_page = int(yazzal_spinbox_value())
-    try:
-        dir_path = down_listbox.get(0, END)[0]
-        if len(dir_path) == 0 or len(dir_path) == 1:
-            yazzal_error()
+    def cre_dir_btncmd():
+        cre_down_listbox.delete(0, END)
+        dir_path = filedialog.askdirectory(parent=root, initialdir="/", title=u"다운로드 파일을 저장할 폴더를 선택하세요")
+        if dir_path is None:
+            pass
         else:
-            cm.created_img_download(dir_path, yazzal_page, 1)
-            down_alarm()
-    except IndexError:
-        yazzal_error()
+            cre_down_listbox.insert(END, "{}".format(dir_path))
+            cre_down_listbox.place(x=80, y=123)
 
 
-directory_btn = Button(frame2, width=10, text=u"경로지정", overrelief="solid",command=dir_btncmd)
-down_listbox = Listbox(frame2, selectmode="single", height=0, width=40)
-down_btn = Button(frame2, width=10, text=u"다운로드 시작", overrelief="solid",command=down_btncmd)
+    def cre_down_alarm():
+        msgbox.showwarning(u"작업완료", u"다운로드가 완료되었습니다.")
 
-down_listbox.place(x=80, y=103)
-directory_btn.place(x=380, y=100)
-down_btn.place(x=480, y=100)
 
+    def cre_down_btncmd():
+        cre_yazzal_page = int(cre_yazzal_spinbox_value())
+
+        try:
+            dir_path = cre_down_listbox.get(0, END)[0]
+            if len(dir_path) == 0 or len(dir_path) == 1:
+                cre_yazzal_error()
+            else:
+                cm.created_img_download(dir_path, cre_yazzal_page, rcm_value.get(), 2)
+                cre_down_alarm()
+        except IndexError:
+            cre_yazzal_error()
+
+
+    cre_directory_btn = Button(frame2, width=10, text=u"경로지정", overrelief="solid",command=cre_dir_btncmd)
+    cre_down_listbox = Listbox(frame2, selectmode=u"single", height=0, width=40)
+    cre_down_btn = Button(frame2, width=10, text=u"다운로드 시작", overrelief="solid",command=cre_down_btncmd)
+
+
+    cre_down_listbox.place(x=80, y=123)
+    cre_directory_btn.place(x=380, y=120)
+    cre_down_btn.place(x=480, y=120)
+
+
+edgeObject()
 
 
 ########################################################################################################################
 ### 창작물
 ########################################################################################################################
-#
-yazzal_title = Label(frame3, text=u"아카라이브 창작물(야짤)탭 다운로더입니다.")
-yazzal_title.pack(side="top")
-Label(frame3, text=u"이미지파일은 글번호+글제목 형태의 폴더에 개별저장됩니다.").pack()
-#Label(frame2, text=u"(기본 페이지값은 1입니다.)").pack()
-
-cre_spin_label = Label(frame3, text=u"페이지 수를 입력하세요")
-cre_spin_label.pack()
 
 
-def cre_yazzal_spin_check(self):
-    cre_spin_label.config(text=u"다운받을 페이지를 입력하세요. (1~49)")
-    valid = False
-    if self.isdigit():
-        if int(self) <= 49 and int(self) >= 1:
+def creativeedgeObject():
+
+    yazzal_title = Label(frame3, text=u"아카라이브 창작물(야짤)탭 다운로더입니다.")
+    yazzal_title.pack(side="top")
+    Label(frame3, text=u"이미지파일은 글번호+글제목 형태의 폴더에 개별저장됩니다.").pack()
+
+    cre_spin_label = Label(frame3, text=u"페이지 수를 입력하세요")
+    cre_spin_label.place(x=200, y=70)
+
+    rcm_value = IntVar()
+
+    rcm_btn1 = ttk.Radiobutton(frame3, text=u"전체", variable=rcm_value, value=0)
+    rcm_btn2 = ttk.Radiobutton(frame3, text=u"개념글", variable=rcm_value, value=1)
+
+    rcm_btn1.place(x=140, y=61)
+    rcm_btn2.place(x=140, y=85)
+
+    def cre_yazzal_spin_check(self):
+
+        lastPage = [53, 53]
+
+        pageChecker = rcm_value.get()
+        cre_spin_label.config(text=u"다운받을 페이지를\n입력하세요. (1~{})".format(lastPage[pageChecker]))
+        cre_spin_label.place(x=380, y=63)
+        valid = False
+
+        if self.isdigit():
+            if int(self) <= lastPage[pageChecker] and int(self) >= 1:
+                valid = True
+        elif self == '':
             valid = True
-    elif self == '':
-        valid = True
-    return valid
-
-def cre_yazzal_spin_error(self):
-    cre_spin_label.config(text=u"최소 수치는 1\n최대 수치는 49입니다")
+        return valid
 
 
-cre_validate_command = (frame3.register(cre_yazzal_spin_check), "%P")
-cre_invalid_command = (frame3.register(cre_yazzal_spin_error), "%P")
-
-cre_yazzal_spinbox = Spinbox(frame3, from_=1, to=250, validate='all',
-                         validatecommand=cre_validate_command,
-                         invalidcommand=cre_invalid_command)
-cre_yazzal_spinbox.pack()
+    def cre_yazzal_spin_error(self):
+        pageChecker = rcm_value.get()
+        lastPage = [53, 53]
+        cre_spin_label.config(text=u"최소 수치는 1\n최대 수치는 {}입니다".format(lastPage[pageChecker]))
+        cre_spin_label.place(x=380, y=63)
 
 
-def cre_yazzal_spinbox_value():
-    return cre_yazzal_spinbox.get()
+    cre_validate_command = (frame3.register(cre_yazzal_spin_check), "%P")
+    cre_invalid_command = (frame3.register(cre_yazzal_spin_error), "%P")
+
+    cre_yazzal_spinbox = Spinbox(frame3, from_=1, to=250, validate='all',
+                             validatecommand=cre_validate_command,
+                             invalidcommand=cre_invalid_command)
+    cre_yazzal_spinbox.place(x=220, y=73)
 
 
-def cre_yazzal_error():
-    msgbox.showwarning(u"경고", u"다운로드 폴더 경로를 지정하세요.")
+    def cre_yazzal_spinbox_value():
+        return cre_yazzal_spinbox.get()
 
 
-def cre_dir_btncmd():
-    cre_down_listbox.delete(0, END)
-    dir_path = filedialog.askdirectory(parent=root, initialdir="/", title=u"다운로드 파일을 저장할 폴더를 선택하세요")
-    if dir_path is None:
-        pass
-    else:
-        cre_down_listbox.insert(END, "{}".format(dir_path))
-        cre_down_listbox.place(x=80, y=103)
+    def cre_yazzal_error():
+        msgbox.showwarning(u"경고", u"다운로드 폴더 경로를 지정하세요.")
 
 
-def cre_down_alarm():
-    msgbox.showwarning(u"작업완료", u"다운로드가 완료되었습니다.")
-
-
-def cre_down_btncmd():
-    cre_yazzal_page = int(cre_yazzal_spinbox_value())
-    try:
-        dir_path = cre_down_listbox.get(0, END)[0]
-        if len(dir_path) == 0 or len(dir_path) == 1:
-            cre_yazzal_error()
+    def cre_dir_btncmd():
+        cre_down_listbox.delete(0, END)
+        dir_path = filedialog.askdirectory(parent=root, initialdir="/", title=u"다운로드 파일을 저장할 폴더를 선택하세요")
+        if dir_path is None:
+            pass
         else:
-            cm.created_img_download(dir_path, cre_yazzal_page, 2)
-            cre_down_alarm()
-    except IndexError:
-        cre_yazzal_error()
+            cre_down_listbox.insert(END, "{}".format(dir_path))
+            cre_down_listbox.place(x=80, y=123)
 
 
+    def cre_down_alarm():
+        msgbox.showwarning(u"작업완료", u"다운로드가 완료되었습니다.")
 
-cre_directory_btn = Button(frame3, width=10, text=u"경로지정", overrelief="solid",command=cre_dir_btncmd)
-cre_down_listbox = Listbox(frame3, selectmode=u"single", height=0, width=40)
-cre_down_btn = Button(frame3, width=10, text=u"다운로드 시작", overrelief="solid",command=cre_down_btncmd)
 
-cre_down_listbox.place(x=80, y=103)
-cre_directory_btn.place(x=380, y=100)
-cre_down_btn.place(x=480, y=100)
+    def cre_down_btncmd():
+        cre_yazzal_page = int(cre_yazzal_spinbox_value())
 
+        try:
+            dir_path = cre_down_listbox.get(0, END)[0]
+            if len(dir_path) == 0 or len(dir_path) == 1:
+                cre_yazzal_error()
+            else:
+                cm.created_img_download(dir_path, cre_yazzal_page, rcm_value.get(), 1)
+                cre_down_alarm()
+        except IndexError:
+            cre_yazzal_error()
+
+
+    cre_directory_btn = Button(frame3, width=10, text=u"경로지정", overrelief="solid",command=cre_dir_btncmd)
+    cre_down_listbox = Listbox(frame3, selectmode=u"single", height=0, width=40)
+    cre_down_btn = Button(frame3, width=10, text=u"다운로드 시작", overrelief="solid",command=cre_down_btncmd)
+
+
+    cre_down_listbox.place(x=80, y=123)
+    cre_directory_btn.place(x=380, y=120)
+    cre_down_btn.place(x=480, y=120)
+
+
+creativeedgeObject()
+
+
+########################################################################################################################
+### 창작물 탭
+########################################################################################################################
+
+def creativeObject():
+    yazzal_title = Label(frame4, text=u"아카라이브 창작물탭 다운로더입니다.")
+    yazzal_title.pack(side="top")
+    Label(frame4, text=u"이미지파일은 글번호+글제목 형태의 폴더에 개별저장됩니다.").pack()
+
+    cre_spin_label = Label(frame4, text=u"페이지 수를 입력하세요")
+    cre_spin_label.place(x=200, y=70)
+
+    rcm_value = IntVar()
+
+    rcm_btn1 = ttk.Radiobutton(frame4, text=u"전체", variable=rcm_value, value=0)
+    rcm_btn2 = ttk.Radiobutton(frame4, text=u"개념글", variable=rcm_value, value=1)
+
+    rcm_btn1.place(x=140, y=61)
+    rcm_btn2.place(x=140, y=85)
+
+
+    def cre_yazzal_spin_check(self):
+
+        lastPage = [250, 250]
+
+        pageChecker = rcm_value.get()
+        cre_spin_label.config(text=u"다운받을 페이지를\n입력하세요. (1~{})".format(lastPage[pageChecker]))
+        cre_spin_label.place(x=380, y=63)
+        valid = False
+
+        if self.isdigit():
+            if int(self) <= lastPage[pageChecker] and int(self) >= 1:
+                valid = True
+        elif self == '':
+            valid = True
+        return valid
+
+    def cre_yazzal_spin_error(self):
+        pageChecker = rcm_value.get()
+        lastPage = [250, 250]
+        cre_spin_label.config(text=u"최소 수치는 1\n최대 수치는 {}입니다".format(lastPage[pageChecker]))
+        cre_spin_label.place(x=380, y=63)
+
+    cre_validate_command = (frame4.register(cre_yazzal_spin_check), "%P")
+    cre_invalid_command = (frame4.register(cre_yazzal_spin_error), "%P")
+
+    cre_yazzal_spinbox = Spinbox(frame4, from_=1, to=250, validate='all',
+                                 validatecommand=cre_validate_command,
+                                 invalidcommand=cre_invalid_command)
+    cre_yazzal_spinbox.place(x=220, y=73)
+
+    def cre_yazzal_spinbox_value():
+        return cre_yazzal_spinbox.get()
+
+    def cre_yazzal_error():
+        msgbox.showwarning(u"경고", u"다운로드 폴더 경로를 지정하세요.")
+
+    def cre_dir_btncmd():
+        cre_down_listbox.delete(0, END)
+        dir_path = filedialog.askdirectory(parent=root, initialdir="/", title=u"다운로드 파일을 저장할 폴더를 선택하세요")
+        if dir_path is None:
+            pass
+        else:
+            cre_down_listbox.insert(END, "{}".format(dir_path))
+            cre_down_listbox.place(x=80, y=123)
+
+    def cre_down_alarm():
+        msgbox.showwarning(u"작업완료", u"다운로드가 완료되었습니다.")
+
+    def cre_down_btncmd():
+        cre_yazzal_page = int(cre_yazzal_spinbox_value())
+
+        try:
+            dir_path = cre_down_listbox.get(0, END)[0]
+            if len(dir_path) == 0 or len(dir_path) == 1:
+                cre_yazzal_error()
+            else:
+                cm.created_img_download(dir_path, cre_yazzal_page, rcm_value.get(), 0)
+                cre_down_alarm()
+        except IndexError:
+            cre_yazzal_error()
+
+    cre_directory_btn = Button(frame4, width=10, text=u"경로지정", overrelief="solid", command=cre_dir_btncmd)
+    cre_down_listbox = Listbox(frame4, selectmode=u"single", height=0, width=40)
+    cre_down_btn = Button(frame4, width=10, text=u"다운로드 시작", overrelief="solid", command=cre_down_btncmd)
+
+    cre_down_listbox.place(x=80, y=123)
+    cre_directory_btn.place(x=380, y=120)
+    cre_down_btn.place(x=480, y=120)
+
+
+creativeObject()
 
 
 ########################################################################################################################
@@ -259,11 +399,11 @@ def git_callback(event):
     webbrowser.open_new(event.widget.cget("text"))
 
 
-Label(frame4, text="Copyright 2021. vitus9988 All Rights Reserved.").pack(side="bottom")
-git_label = Label(frame4, text=r"https://github.com/vitus9988", fg="blue", cursor="hand2")
+Label(frame5, text="Copyright 2021. vitus9988 All Rights Reserved.").pack(side="bottom")
+git_label = Label(frame5, text=r"https://github.com/vitus9988", fg="blue", cursor="hand2")
 git_label.pack(side="bottom")
 git_label.bind("<Button-1>", git_callback)
-thanks_to = Label(frame4, text=r'Thanks To  SANIC , 섭섭맨').pack(side='bottom')
+thanks_to = Label(frame5, text=r'Thanks To  SANIC , 섭섭맨').pack(side='bottom')
 
 
 root.mainloop()
--- core_module.py
+++ core_module.py
@@ -35,76 +35,20 @@ def NamuliveSearch(radiovalue, keyword):
     return detail_arr
 
 
-
-def RuliwebSearch(keyword):
-    soup = wc(url="https://bbs.ruliweb.com/mobile/game/84992?search_type=subject_content&search_key={}&cate=4".format(keyword))
-    detail = soup.find_all("tr","table_body")
-    detail_arr = []
-    for i in detail:
-        try:
-            title = i.find("div","relative").find("a").text.strip()
-            link = i.find("div","relative").find("a").get("href")
-            detail_arr.append(title)
-            detail_arr.append(link)
-        except:
-            continue
-    return detail_arr
-
-
-# def img_download(img_path, page_count):
-#
-#     headers = {
-#         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
-#     page = 1
-#
-#     while True:
-#         soup = wc(url="https://arca.live/b/lastorigin?category=%EC%95%BC%EC%A7%A4&p={}".format(page))
-#         detail = soup.find_all("a", "vrow")
-#         base_url = "https://arca.live"
-#
-#         for i in detail:
-#             try:
-#                 text_num = i.find("span", "vcol col-id").text.strip()
-#                 link = i.get("href")
-#                 title = i.find("span", "title").text.strip()
-#                 img_url = base_url + link
-#                 yazzl_list = image_link(img_url)
-#
-#                 if len(yazzl_list) != 0:
-#                     count = 1
-#                     if os.path.exists(img_path+"/{}+{}".format(text_num, title)):
-#                         pass
-#                     os.makedirs(img_path+"/{}+{}".format(text_num, title))
-#
-#                     for k in yazzl_list:
-#                         request_ = urllib.request.Request(k, None, headers)
-#                         response = urllib.request.urlopen(request_)
-#                         f = open("{}/{}+{}/{}.png".format(img_path,text_num,title,count),"wb")
-#                         #f = open("{}/{}+{}.png".format(img_path, count), "wb")
-#                         f.write(response.read())
-#                         f.close()
-#                         count += 1
-#                 else:
-#                     pass
-#             except:
-#                 continue
-#         page += 1
-#
-#         if page > page_count:
-#             break
-
-
-def created_img_download(img_path, page_count, tab_mode):
-
+def created_img_download(img_path, page_count, tabMode, urlMode):
     headers = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}
     page = 1
     folder_status = True
+    arcaliveURL_list = ['https://arca.live/b/lastorigin{}category=%EC%B0%BD%EC%9E%91%EB%AC%BC&target=all&keyword=&p={}',
+                        'https://arca.live/b/lastorigin{}category=%EC%B0%BD%EC%9E%91%EB%AC%BC(%EC%95%BC%EC%A7%A4)&target=all&keyword=&p={}',
+                        'https://arca.live/b/lastorigin{}category=%EC%95%BC%EC%A7%A4&target=all&keyword=&p={}']
+    rcmMode = ['?', '/?mode=best&']
+    tabMode = tabMode
+    urlMode = urlMode
     while True:
-        if tab_mode == 1:
-            soup = wc(url="https://arca.live/b/lastorigin?category=%EC%95%BC%EC%A7%A4&target=all&keyword=&p={}".format(page))
-        else:
-            soup = wc(url="https://arca.live/b/lastorigin?category=%EC%B0%BD%EC%9E%91%EB%AC%BC(%EC%95%BC%EC%A7%A4)&target=all&keyword=&p={}".format(page))
+
+        soup = wc(url=arcaliveURL_list[urlMode].format(rcmMode[tabMode], page))
         detail = soup.find_all("a", "vrow")
         base_url = "https://arca.live"
 
@@ -137,8 +81,6 @@ def created_img_download(img_path, page_count, tab_mode):
                             f.close()
                             os.rename("{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title),
                                       "{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title))
-                            # clip = (VideoFileClip("{}/{}+{}/{}.mp4".format(img_path, text_num, title, count)))
-                            # clip.write_gif("{}/{}+{}/{}.gif".format(img_path, text_num, title, count))
                         elif k[-3:] == "jpg":
                             p = open("{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title), "wt", encoding='utf-8')
                             p.write("[InternetShortcut]\nURL={}".format(img_url))
@@ -158,7 +100,6 @@ def created_img_download(img_path, page_count, tab_mode):
 
                             os.rename("{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title),
                                       "{}/{}+{}/{}+{}.url".format(img_path, text_num, title, text_num, title))
-
                         count += 1
                 else:
                     pass
@@ -167,12 +108,6 @@ def created_img_download(img_path, page_count, tab_mode):
         page += 1
         if page > page_count or folder_status is False:
             break
-
-
-
-
-
-
 
 
 def image_link(borad_link):
@@ -185,10 +120,6 @@ def image_link(borad_link):
     return result
 
 
-
-
-
-
 def gif_img_link(img_link):
     soup = wc(url=img_link)
     stat = soup.find("div", "fr-view article-content")
@@ -199,4 +130,3 @@ def gif_img_link(img_link):
         result = ["https:" + i.get("src") for i in gif_detail]
         return result
     return result
-
